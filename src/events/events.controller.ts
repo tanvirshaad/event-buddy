91e6dd3fce322892209c7a2a72de2acda3aa0764
tanvirshaad <tanvirshaad@gmail.com>
2025-05-25 14:55:10 +0600

    events api added

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, ParseIntPipe } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { Get, Post, Body } from '@nestjs/common';
 import { CreateEventDto } from './dtos/create-event.dto';
@@ -7,13 +7,28 @@ import { CreateEventDto } from './dtos/create-event.dto';
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
+  @Post()
+  public async createEvent(@Body() createEventDto: CreateEventDto) {
+    return this.eventsService.createEvent(createEventDto);
+  }
+
   @Get()
   public async getAllEvents() {
     return this.eventsService.getAllEvents();
   }
 
-  @Post()
-  public async createEvent(@Body() eventData: CreateEventDto) {
-    return this.eventsService.createEvent(eventData);
+  @Get('/upcoming-events')
+  public async getUpcomingEvents() {
+    return this.eventsService.getUpcomingEvents();
+  }
+
+  @Get('/past-events')
+  public async getPastEvents() {
+    return this.eventsService.getPastEvents();
+  }
+
+  @Get(':id')
+  public async getEventDetails(@Param('id', ParseIntPipe) eventId: number) {
+    return this.eventsService.getEventDetails(eventId);
   }
 }
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,6 +1,6 @@
 import { Body, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { LessThan, MoreThanOrEqual, Repository } from 'typeorm';
 import { Event } from './event.entity';
 import { CreateEventDto } from './dtos/create-event.dto';
 
@@ -11,12 +11,55 @@ export class EventsService {
     private readonly eventRepository: Repository<Event>,
   ) {}
 
-  public async createEvent(@Body() eventData: CreateEventDto): Promise<Event> {
-    const event = this.eventRepository.create(eventData);
-    return this.eventRepository.save(event);
+  public async createEvent(
+    @Body() createEventDto: CreateEventDto,
+  ): Promise<Event> {
+    const newEvent = this.eventRepository.create({
+      ...createEventDto,
+      date: new Date(createEventDto.date),
+      bookedSeats: 0,
+    });
+    return this.eventRepository.save(newEvent);
   }
 
   public async getAllEvents(): Promise<Event[]> {
     return this.eventRepository.find();
+  }
+
+  //get upcoming events
+  public async getUpcomingEvents(): Promise<Event[]> {
+    const currentDate = new Date();
+    return this.eventRepository.find({
+      where: {
+        date: MoreThanOrEqual(currentDate),
+      },
+      order: {
+        date: 'ASC',
+      },
+    });
+  }
+
+  //get past events
+  public async getPastEvents(): Promise<Event[]> {
+    const currentDate = new Date();
+    return this.eventRepository.find({
+      where: {
+        date: LessThan(currentDate),
+      },
+      order: {
+        date: 'DESC',
+      },
+    });
+  }
+
+  //get details of a specific event
+  public async getEventDetails(eventId: number): Promise<Event> {
+    const event = await this.eventRepository.findOne({
+      where: { id: eventId },
+    });
+    if (!event) {
+      throw new Error(`Event with ID ${eventId} not found`);
+    }
+    return event;
   }
 }
